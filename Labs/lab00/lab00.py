5baac1ce1096f143c54a0d6f96b4c09ab5f5e82e
yinchaoyang <YCY2857959697@hotmail.com>
2024-02-23 16:04:31 +0800

    modify .gitignore : add .ok_* to prevent my ok history from uploading ; complete week 01 : disc00, lab00, hw01

---
+++ Labs/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_three():
+    """Come up with the most creative expression that evaluates to 2023,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_three()
+    2023
+    """
+    return 2 ** 11 - 2 ** 4 - 3 ** 2
+
