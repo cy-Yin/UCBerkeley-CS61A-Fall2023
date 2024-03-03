f6305b9f070c3750aee01a7dec4e72f9835bdc1d
yinchaoyang <YCY2857959697@hotmail.com>
2024-03-03 19:35:22 +0800

    complete project02 cats

--- Projects/02_cats/cats.py
+++ Projects/02_cats/cats.py
@@ -31,6 +31,14 @@ def pick(paragraphs, select, k):
     """
     # BEGIN PROBLEM 1
     "*** YOUR CODE HERE ***"
+    lst = []
+    for elem in paragraphs:
+        if select(elem) == True:
+            lst = lst + [elem]
+    if k >= len(lst):
+        return ''
+    else:
+        return lst[k]
     # END PROBLEM 1
 
 
@@ -50,6 +58,14 @@ def about(subject):
     assert all([lower(x) == x for x in subject]), 'subjects should be lowercase.'
     # BEGIN PROBLEM 2
     "*** YOUR CODE HERE ***"
+    def contain(paragraph):
+        modified_paragraph = split(lower(remove_punctuation(paragraph)))
+        for elem in modified_paragraph:
+            for checker in subject: 
+                if checker == elem: 
+                    return True
+        return False
+    return contain
     # END PROBLEM 2
 
 
@@ -80,6 +96,18 @@ def accuracy(typed, source):
     source_words = split(source)
     # BEGIN PROBLEM 3
     "*** YOUR CODE HERE ***"
+    if typed_words == [] and source_words == []:
+        return 100.0
+    elif typed_words == [] and source_words != []:
+        return 0.0
+    else:
+        score = 0
+        for index in range(min(len(typed_words), len(source_words))):
+            if typed_words[index] == source_words[index]:
+                score = score + 1
+        accurate_rate = score / len(typed_words) * 100
+        return accurate_rate
+
     # END PROBLEM 3
 
 
@@ -98,6 +126,7 @@ def wpm(typed, elapsed):
     assert elapsed > 0, 'Elapsed time must be positive'
     # BEGIN PROBLEM 4
     "*** YOUR CODE HERE ***"
+    return (len(typed) / 5) / elapsed * 60
     # END PROBLEM 4
 
 
@@ -127,6 +156,22 @@ def autocorrect(typed_word, word_list, diff_function, limit):
     """
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+    if typed_word in word_list:
+        return typed_word
+    else:
+        lst_diff = []
+        for check_word in word_list:
+            lst_diff = lst_diff + [diff_function(typed_word, check_word, limit)]
+        if min(lst_diff, key=abs) > limit: # No correct word
+            return typed_word
+        else:
+            lowest_diff_index = 0
+            lowest_diff = lst_diff[0]
+            for index in range(len(lst_diff)):
+                if lst_diff[index] < lowest_diff:
+                    lowest_diff_index = index
+                    lowest_diff = lst_diff[index]
+            return word_list[lowest_diff_index]
     # END PROBLEM 5
 
 
@@ -153,7 +198,157 @@ def feline_fixes(typed, source, limit):
     5
     """
     # BEGIN PROBLEM 6
-    assert False, 'Remove this line'
+    '''
+    The code below has passed all the tests except suite 1 case 6, which is described as follows.
+
+    > from cats import feline_fixes, autocorrect
+    > import tests.construct_check as test
+    > # Check that the recursion stops when the limit is reached
+    > import trace, io
+    > from contextlib import redirect_stdout
+    > with io.StringIO() as buf, redirect_stdout(buf):
+    .     trace.Trace(trace=True).runfunc(feline_fixes, "someaweqwertyuio", "awesomeasdfghjkl", 3)
+    .     output = buf.getvalue()
+    > len([line for line in output.split('\n') if 'funcname' in line]) < 12
+    False
+
+    Why is that? 
+    > for line in output.split('\n'):
+    .     print(line)
+    .
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "someaweqwertyuio", "awesomeasdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "omeaweqwertyuio", "wesomeasdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "meaweqwertyuio", "esomeasdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "eaweqwertyuio", "someasdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "aweqwertyuio", "omeasdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "weqwertyuio", "measdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "eqwertyuio", "easdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(207):             return feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "qwertyuio", "asdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "wertyuio", "sdfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "ertyuio", "dfghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "rtyuio", "fghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "tyuio", "ghjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "yuio", "hjkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "uio", "jkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "uio", "jkl"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "o", "l"
+    cats.py(204):         typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+    cats.py(205):         source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+    cats.py(206):         if typed_check == source_check:
+    cats.py(209):             return 1 + feline_fixes(new_typed, new_source, limit)
+    --- modulename: cats, funcname: feline_fixes
+    cats.py(201):     if typed == "" or source == "": # "", ""
+    cats.py(202):         return max(len(typed), len(source))
+    15
+
+    The function has computed the whole diff between two words. However, if the diff has been larger than limit, 
+    we do not need to know whether the diff is (limit + 1) or (limit + 100). We just return (limit + 1) to give 
+    the message that the diff is too large to autocorrect, rather than computing the whole recursion, which will 
+    minimize the extra computation.
+
+    Original Code:
+    ```
+    if typed == "" or source == "":
+        return max(len(typed), len(source))
+    else:
+        typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+        source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+        if typed_check == source_check:
+            return feline_fixes(new_typed, new_source, limit)
+        else:
+            return 1 + feline_fixes(new_typed, new_source, limit)
+    ```
+    '''
+    if typed == "" or source == "":
+        return max(len(typed), len(source))
+    elif limit == 0:
+        if typed == source:
+            return 0
+        else:
+            return 1
+    else:
+        typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+        source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+        if typed_check == source_check:
+            return feline_fixes(new_typed, new_source, limit)
+        else:
+            return 1 + feline_fixes(new_typed, new_source, limit - 1)
     # END PROBLEM 6
 
 
@@ -177,31 +372,58 @@ def minimum_mewtations(typed, source, limit):
     >>> minimum_mewtations("ckiteus", "kittens", big_limit) # ckiteus -> kiteus -> kitteus -> kittens
     3
     """
-    assert False, 'Remove this line'
-    if ___________: # Base cases should go here, you may add more base cases as needed.
-        # BEGIN
-        "*** YOUR CODE HERE ***"
-        # END
     # Recursive cases should go below here
-    if ___________: # Feel free to remove or add additional cases
-        # BEGIN
-        "*** YOUR CODE HERE ***"
-        # END
+    if typed == "" or source == "":
+        return max(len(typed), len(source))
+    elif limit == 0:
+        if typed == source:
+            return 0
+        else:
+            return 1
     else:
-        add = ... # Fill in these lines
-        remove = ...
-        substitute = ...
-        # BEGIN
-        "*** YOUR CODE HERE ***"
-        # END
+        typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+        source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+        if typed_check == source_check:
+            return minimum_mewtations(new_typed, new_source, limit)
+        else:
+            add = 1 + minimum_mewtations(source[0] + typed, source, limit - 1) # Fill in these lines
+            remove = 1 + minimum_mewtations(typed[1:], source, limit - 1)
+            substitute = 1 + minimum_mewtations(typed[1:], source[1:], limit - 1)
+            # BEGIN
+            "*** YOUR CODE HERE ***"
+            return min(add, remove, substitute)
+            # END
+    
 
 
 def final_diff(typed, source, limit):
     """A diff function that takes in a string TYPED, a string SOURCE, and a number LIMIT.
     If you implement this function, it will be used."""
-    assert False, 'Remove this line to use your final_diff function.'
+    assert False
+    '''The diff function as follows does not have a good autocorrect effect. Sad!!!'''
+    if typed == "" or source == "":
+        return max(len(typed), len(source))
+    elif limit == 0:
+        if typed == source:
+            return 0
+        else:
+            return 1
+    else:
+        typed_check, new_typed = typed[0], (typed[1:] if len(typed) >= 1 else "")
+        source_check, new_source = source[0], (source[1:] if len(source) >= 1 else "")
+        if typed_check == source_check:
+            return final_diff(new_typed, new_source, limit)
+        else:
+            add = 1 + final_diff(source[0] + typed, source, limit - 1) # Fill in these lines
+            remove = 1 + final_diff(typed[1:], source, limit - 1)
+            substitute = 1 + final_diff(typed[1:], source[1:], limit - 1)
+            if len(typed) >= 2:
+                exchange = 1 + final_diff(typed[0] + typed[1] + typed[2:], source, limit - 1)
+                return min(add, remove, substitute, exchange)
+            else:
+                return min(add, remove, substitute)
 
-FINAL_DIFF_LIMIT = 6 # REPLACE THIS WITH YOUR LIMIT
+FINAL_DIFF_LIMIT = 4 # REPLACE THIS WITH YOUR LIMIT
 
 
 ###########
@@ -234,6 +456,15 @@ def report_progress(typed, source, user_id, upload):
     """
     # BEGIN PROBLEM 8
     "*** YOUR CODE HERE ***"
+    total_num = len(source)
+    report = {'id': user_id, 'progress': 0.0}
+    index = 0
+    while index < min(len(typed), len(source)) and typed[index] == source[index]:
+        index = index + 1
+    report['progress'] = index / total_num
+    upload(report)
+    
+    return report['progress']
     # END PROBLEM 8
 
 
@@ -256,6 +487,12 @@ def time_per_word(words, timestamps_per_player):
     """
     # BEGIN PROBLEM 9
     "*** YOUR CODE HERE ***"
+    times = []
+    for per_timestamp in timestamps_per_player:
+        per_interval = [(per_timestamp[i] - per_timestamp[i - 1]) for i in range(1, len(per_timestamp))]
+        times += [per_interval]
+
+    return match(words, times)
     # END PROBLEM 9
 
 
@@ -278,6 +515,17 @@ def fastest_words(match):
     word_indices = range(len(get_all_words(match)))    # contains an *index* for each word
     # BEGIN PROBLEM 10
     "*** YOUR CODE HERE ***"
+    lst_fastest_word = [[] for _ in player_indices]
+    for word_indice in word_indices:
+        index = 0
+        fastest_player_time = time(match, 0, word_indice)
+        for player_indice in player_indices:
+            if time(match, player_indice, word_indice) < fastest_player_time:
+                index = player_indice
+                fastest_player_time = time(match, player_indice, word_indice)
+        lst_fastest_word[index] += [get_word(match, word_indice)]
+    
+    return lst_fastest_word
     # END PROBLEM 10
 
 
@@ -326,7 +574,7 @@ def match_string(match):
     """A helper function that takes in a match data abstraction and returns a string representation of it"""
     return f"match({get_all_words(match)}, {get_all_times(match)})"
 
-enable_multiplayer = False  # Change to True when you're ready to race.
+enable_multiplayer = True  # Change to True when you're ready to race.
 
 ##########################
 # Command Line Interface #
